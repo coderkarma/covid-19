47add5f0da53fbc4c7964e812bdd8b6f6787eccd
Karma Drukpa <karmadrukpa11@gmail.com>
2020-07-22 23:23:59 -0400

    Added setInterval in Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,43 +1,79 @@
 import React, { useEffect, useState } from 'react';
-import Counter from './Counter';
+// import Counter from './Counter';
 
 const Card = ({ positive, deaths, recovered, onVentilatorCurrently }) => {
-	// const [counter, setCounter] = useState(0);
+	const [counter, setCounter] = useState(positive - 150);
+	const [counterDeaths, setCounterDeaths] = useState(deaths - 150);
+	// const [counterRecovered, setCounterRecovered] = useState(recovered);
+	const [counterOnVentilator, setCounterOnVentilator] = useState(
+		onVentilatorCurrently
+	);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCounter(counter + 1);
+		}, 1);
+
+		if (counter >= positive) {
+			clearInterval(interval);
+		}
+
+		return () => clearInterval(interval);
+	}, [counter, positive]);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCounterDeaths(counterDeaths + 1);
+		}, 1);
+
+		if (counterDeaths >= deaths) {
+			clearInterval(interval);
+		}
+
+		return () => clearInterval(interval);
+	}, [counterDeaths, deaths]);
 
 	// useEffect(() => {
 	// 	const interval = setInterval(() => {
-	// 		setCounter(counter + 1);
+	// 		setCounterRecovered(counterRecovered + 1);
 	// 	}, 1);
 
-	// 	if (counter >= onVentilatorCurrently) {
+	// 	if (counterRecovered >= recovered) {
 	// 		clearInterval(interval);
 	// 	}
 
 	// 	return () => clearInterval(interval);
-	// }, [counter, onVentilatorCurrently]);
+	// }, [counterRecovered, recovered]);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCounterOnVentilator(counterOnVentilator + 1);
+		}, 1);
+
+		if (counterOnVentilator >= onVentilatorCurrently) {
+			clearInterval(interval);
+		}
+
+		return () => clearInterval(interval);
+	}, [counterOnVentilator, onVentilatorCurrently]);
 
 	return (
 		<>
 			<h2 className='title is-2 has-text-centered has-text-weight-medium'>
 				Coronavirus daily updates
 			</h2>
-			<Counter
-				positive={positive}
-				deaths={deaths}
-				recovere={recovered}
-				onVentilatorCurrently={onVentilatorCurrently}
-			/>
+
 			<nav className='level'>
 				<div className='level-item has-text-centered'>
 					<div>
 						<p className='heading'>Positive</p>
-						<p className='title'>{positive}</p>
+						<p className='title'>{counter}</p>
 					</div>
 				</div>
 				<div className='level-item has-text-centered'>
 					<div>
 						<p className='heading'>Death</p>
-						<p className='title'>{deaths}</p>
+						<p className='title'>{counterDeaths}</p>
 					</div>
 				</div>
 				<div className='level-item has-text-centered'>
@@ -49,7 +85,7 @@ const Card = ({ positive, deaths, recovered, onVentilatorCurrently }) => {
 				<div className='level-item has-text-centered'>
 					<div>
 						<p className='heading'> On Ventilator</p>
-						<p className='title'>{onVentilatorCurrently}</p>
+						<p className='title'>{counterOnVentilator}</p>
 					</div>
 				</div>
 			</nav>
